21dd99b7130855dbc8b249a50c32fa4589669c77
League Student <league@iMac-18.attlocal.net>
2020-01-12 14:30:45 -0800

    IDK what day, so
    Not very productive today! Work harder.

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -17,6 +17,14 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
+JOptionPane.showMessageDialog(null, "If you and Peppa Pig were in a beauty contest, Peppa Pig would win. Oh sorry wrong person.");
+JOptionPane.showMessageDialog(null, "If you are reading this, have a good day!");
+JOptionPane.showMessageDialog(null, "Why did the skeleton go to the party alone? Cuz they had no BODY to go with. Ok i'll walk myself out.");
+JOptionPane.showMessageDialog(null, "You should be proud of yourself.");
+JOptionPane.showMessageDialog(null, "And the Lord said unto John, 'Come forth and you will recieve eternal life.' But john came fifth, and won a toaster.");
+JOptionPane.showMessageDialog(null, "Parallel lines have so much in common. Its a shame they'll never meet.");
+JOptionPane.showMessageDialog(null, "I broke my finger last week. On the other hand, I'm okay.");
+JOptionPane.showMessageDialog(null, "");
 
 		// 2. Repeat all the code above 10 times
 		
